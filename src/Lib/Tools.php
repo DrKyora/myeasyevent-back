6d0f7e04a1d1676ab08a1728bc99c76914f61be2
Remidelgutte <delgutte.remi@hotmail.com>
2025-02-16 15:16:17 +0100

    add function randomString

--- src/Lib/Tools.php
+++ src/Lib/Tools.php
@@ -104,4 +104,15 @@ class Tools
             fclose($logFile);
         }
     }
+
+    function randomString($length)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
 }
